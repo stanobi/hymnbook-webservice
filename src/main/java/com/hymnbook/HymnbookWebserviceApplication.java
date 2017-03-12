88f2b9a8e6b752a594daacf1b9f3896fa0c69709
stanobi <success4stan@gmail.com>
2017-03-12 20:27:41 +0100

    initial phrase

---
+++ src/main/java/com/hymnbook/HymnbookWebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.hymnbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HymnbookWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HymnbookWebserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hymnbook/HymnbookWebserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hymnbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HymnbookWebserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
